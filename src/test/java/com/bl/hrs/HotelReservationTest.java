89e03510b607b6f8137bb362ff3b3092ca377af0
pavan0628 <pavan.cse.rymec@gmail.com>
2025-06-09 17:18:18 +0530

    [PAVAN]ADDED:Added weekday and weekend rates for each hotel

--- src/test/java/com/bl/hrs/HotelReservationTest.java
+++ src/test/java/com/bl/hrs/HotelReservationTest.java
@@ -3,6 +3,7 @@ package com.bl.hrs;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 class HotelReservationTest {
@@ -26,6 +27,31 @@ class HotelReservationTest {
         system.addHotel("Ridgewood", 220, 150); // total: 220 + 220 = 44
 
         assertEquals("Lakewood" ,system.findCheapestHotel(Arrays.asList("10Sep2020", "11Sep2020")).getName());
+    }
+
+    @Test
+    public void testAddHotelswithDifferentRates(){
+
+        system.addHotel("Lakewood", 110, 90);
+        system.addHotel("Bridgewood", 150, 50);
+        system.addHotel("Ridgewood", 220, 150);
+
+        List<Hotel> hotels=system.getHotels();
+
+        assertEquals(3, hotels.size());
+
+        assertEquals("Lakewood", hotels.get(0).getName());
+        assertEquals(110, hotels.get(0).getWeekdayRateRegular());
+        assertEquals(90, hotels.get(0).getWeekendRateRegular());
+
+        assertEquals("Bridgewood", hotels.get(1).getName());
+        assertEquals(150, hotels.get(1).getWeekdayRateRegular());
+        assertEquals(50, hotels.get(1).getWeekendRateRegular());
+
+        assertEquals("Ridgewood", hotels.get(2).getName());
+        assertEquals(220, hotels.get(2).getWeekdayRateRegular());
+        assertEquals(150, hotels.get(2).getWeekendRateRegular());
+
     }
   
 }
