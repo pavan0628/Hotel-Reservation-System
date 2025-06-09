3315f25ed609a6a05718f3948e74222ea7a27d59
pavan0628 <pavan.cse.rymec@gmail.com>
2025-06-09 16:08:22 +0530

    [PAVAN]ADDED: Added ability to find the cheapest hotel for given date range

--- src/main/java/com/bl/hrs/HotelReservation.java
+++ src/main/java/com/bl/hrs/HotelReservation.java
@@ -1,9 +1,14 @@
 package com.bl.hrs;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class HotelReservation {
     private List<Hotel> hotels=new ArrayList<>();
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy", Locale.ENGLISH);
+
     public static void main(String[] args) {
         //printing welecome message
         System.out.println("Welcome to Hotel Reservation Problem!");
@@ -12,6 +17,31 @@ public class HotelReservation {
         Hotel hotel = new Hotel(name, weekdayRate, weekendRate);
         hotels.add(hotel);
     }
+    public Hotel findCheapestHotel(List<String> dateStrings){
+        Map<Hotel,Integer> hotelTotalRates=new HashMap<>();
+        for(Hotel hotel:hotels){
+            Integer total=0;
+            for(String datestr:dateStrings){
+                LocalDate localDate=LocalDate.parse(datestr, formatter);
+                DayOfWeek day=localDate.getDayOfWeek();
+                boolean isWeekend=(day==DayOfWeek.SATURDAY)||(day==DayOfWeek.SUNDAY);
+                total=total+((isWeekend)? hotel.getWeekendRateRegular() : hotel.getWeekdayRateRegular());
+            }
+            hotelTotalRates.put(hotel, total);
+        }
+        Hotel cheapestHotel=null;
+        int minRate=Integer.MAX_VALUE;
+        for(Map.Entry<Hotel,Integer> entry:hotelTotalRates.entrySet()){
+            if(entry.getValue()<minRate) {
+                minRate = entry.getValue();
+                cheapestHotel=entry.getKey();
+            }
+        }
+        System.out.println("The cheapest rate is "+minRate);
+        return cheapestHotel;
+    }
+
+
 
     public List<Hotel> getHotels() {
         return hotels;
--- src/test/java/com/bl/hrs/HotelReservationTest.java
+++ src/test/java/com/bl/hrs/HotelReservationTest.java
@@ -2,12 +2,14 @@ package com.bl.hrs;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 class HotelReservationTest {
-
+    HotelReservation system = new HotelReservation();
     @Test
     public void testAddHotel() {
-        HotelReservation system = new HotelReservation();
+
         system.addHotel("Lakewood", 110, 90);
 
         assertEquals(1, system.getHotels().size());
@@ -16,6 +18,14 @@ class HotelReservationTest {
         assertEquals("Lakewood", hotel.getName());
         assertEquals(110, hotel.getWeekdayRateRegular());
         assertEquals(90, hotel.getWeekendRateRegular());
+    }
+    @Test
+    public void testFindCheapestHotel(){
+        system.addHotel("Lakewood", 110, 90);   // total: 110 + 110 = 220
+        system.addHotel("Bridgewood", 160, 60); // total: 160 + 160 = 320
+        system.addHotel("Ridgewood", 220, 150); // total: 220 + 220 = 44
+
+        assertEquals("Lakewood" ,system.findCheapestHotel(Arrays.asList("10Sep2020", "11Sep2020")).getName());
     }
   
 }
